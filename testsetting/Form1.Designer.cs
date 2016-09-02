ed1903fcd405e180b17fe383db6382b34aede34b
nasa346617 <surayya.nasaruddin@exact.com>
2016-09-02 15:21:16 +0800

    add label

--- testsetting/Form1.Designer.cs
+++ testsetting/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textbx = new System.Windows.Forms.TextBox();
             this.savebtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textbx
@@ -49,11 +50,21 @@
             this.savebtn.UseVisualStyleBackColor = true;
             this.savebtn.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.savebtn);
             this.Controls.Add(this.textbx);
             this.Name = "Form1";
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.TextBox textbx;
         private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Label label1;
     }
 }
 
